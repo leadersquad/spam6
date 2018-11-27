06d6f66b651bdbd6b555858028bfb9c3f7649048
leadersquad <42087381+leadersquad@users.noreply.github.com>
2018-11-27 21:02:52 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+﻿  const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("517026537271328789")
+setInterval(function() {
+channel.send(`salemsalemsalemsalemsalemsalemsalemsalemsalemsalemsalem`);
+}, 30)
+})
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
